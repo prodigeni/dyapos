9290a03d513b0261d256e3483d576c260877716e
Eduardo <edwardoyarzun@openmailbox.org>
2014-03-16 21:11:14 -0300

    Documentation

--- django_app/project/static/js/editor/Slide/SlideOptionsBoxView.js
+++ django_app/project/static/js/editor/Slide/SlideOptionsBoxView.js
@@ -1,5 +1,9 @@
 /**
  * @module Slide
+ */
+
+/**
+ * Panel that shows some options for each slide like edit and delete button, scale, rotation, etc.
  * @class SlideOptionsBoxView
  * @extends Backbone.View
  */
@@ -10,23 +14,72 @@ define([], function() {
 		el : document.getElementById("slide-options"),
 
 		events : {
+			/**
+			 * Calls editSlide()
+			 * @event click #btn-edit-presentation
+			 */
 			"click #btn-edit-presentation" : "editSlide",
+			/**
+			 * Calls scaleSlide()
+			 * @event change #input-scale
+			 */
 			"change #input-scale" : "scaleSlide",
+			/**
+			 * Calls saveScaleSlide()
+			 * @event mouseup #input-scale
+			 */
 			"mouseup #input-scale" : "saveScaleSlide",
+			/**
+			 * Calls rotateSlideZ()
+			 * @event change #input-rotation-z
+			 */
 			"change #input-rotation-z" : "rotateSlideZ",
+			/**
+			 * Calls saveRotateSlideZ()
+			 * @event mouseup #input-rotation-z
+			 */
 			"mouseup #input-rotation-z" : "saveRotateSlideZ",
+			/**
+			 * Calls rotateSlideX()
+			 * @event change #input-rotation-x
+			 */
 			"change #input-rotation-x" : "rotateSlideX",
+			/**
+			 * Calls saveRotateSlideX()
+			 * @event mouseup #input-rotation-x
+			 */
 			"mouseup #input-rotation-x" : "saveRotateSlideX",
+			/**
+			 * Calls rotateSlideY()
+			 * @event change #input-rotation-y
+			 */
 			"change #input-rotation-y" : "rotateSlideY",
+			/**
+			 * Calls saveRotateSlideY()
+			 * @event mouseup #input-rotation-y
+			 */
 			"mouseup #input-rotation-y" : "saveRotateSlideY",
+			/**
+			 * Calls deleteSlide()
+			 * @event click #btn-delete-slide
+			 */
 			"click #btn-delete-slide" : "deleteSlide"
 		},
 
+		/**
+		 * Goes to the edit mode on the selected slide
+		 * @method editSlide
+		 */
 		editSlide : function() {
 			app.views.navigation_mode.exitMode();
 			app.views.edit_mode.enterMode();
 		},
 
+		/**
+		 * Scales the slide
+		 * @method scaleSlide
+		 * @param {Object} event Change event
+		 */
 		scaleSlide : function(event) {
 			var scale = event.target.value,
 				style = app.clicked_slide.style[app.css_transform];
@@ -36,6 +89,11 @@ define([], function() {
 			app.clicked_slide.dataset.scale = scale;
 		},
 
+		/**
+		 * Saves the scale value
+		 * @method scaleSlide
+		 * @param {Object} event Mouseup event
+		 */
 		saveScaleSlide : function() {
 			var scale = document.getElementById(app.selected_slide).dataset.scale,
 				slide = document.getElementById(app.selected_slide);
@@ -46,6 +104,11 @@ define([], function() {
 			app.slides.get(app.selected_slide).set("scale", scale);
 		},
 
+		/**
+		 * Rotates the slide on the Z-axis
+		 * @method rotateSlideZ
+		 * @param {Object} event Change event
+		 */
 		rotateSlideZ : function(event) {
 			var degreesZ = event.target.value,
 				slide = app.clicked_slide,
@@ -56,6 +119,11 @@ define([], function() {
 			slide.dataset.rotateZ = degreesZ;
 		},
 
+		/**
+		 * Saves the rotation on the Z-axis
+		 * @method saveRotateSlideZ
+		 * @param {Object} event Mouseup event
+		 */
 		saveRotateSlideZ : function() {
 			var degrees = parseInt(document.getElementById(app.selected_slide).dataset.rotateZ, 10),
 				slide = document.getElementById(app.selected_slide);
@@ -65,6 +133,11 @@ define([], function() {
 			app.slides.get(app.selected_slide).set("rotation_z", degrees);
 		},
 
+		/**
+		 * Rotates the slide on the X-axis
+		 * @method rotateSlideX
+		 * @param {Object} event Change event
+		 */
 		rotateSlideX : function(event) {
 			var degreesX = event.target.value,
 				slide = app.clicked_slide,
@@ -75,6 +148,11 @@ define([], function() {
 			slide.dataset.rotateX = degreesX;
 		},
 
+		/**
+		 * Saves the rotation on the X-axis
+		 * @method saveRotateSlideX
+		 * @param {Object} event Mouseup event
+		 */
 		saveRotateSlideX : function() {
 			var degrees = parseInt(document.getElementById(app.selected_slide).dataset.rotateX, 10),
 				slide = document.getElementById(app.selected_slide);
@@ -84,6 +162,11 @@ define([], function() {
 			app.slides.get(app.selected_slide).set("rotation_x", degrees);
 		},
 
+		/**
+		 * Rotates the slide on the Y-axis
+		 * @method rotateSlideY
+		 * @param {Object} event Change event
+		 */
 		rotateSlideY : function(event) {
 			var degreesY = event.target.value,
 				slide = document.getElementById(app.selected_slide),
@@ -94,6 +177,11 @@ define([], function() {
 			slide.dataset.rotateY = degreesY;
 		},
 
+		/**
+		 * Saves the rotation on the Y-axis
+		 * @method saveRotateSlideY
+		 * @param {Object} event Mouseup event
+		 */
 		saveRotateSlideY : function() {
 			var degrees = parseInt(document.getElementById(app.selected_slide).dataset.rotateY, 10),
 				slide = document.getElementById(app.selected_slide);
@@ -103,15 +191,15 @@ define([], function() {
 			app.slides.get(app.selected_slide).set("rotation_y", degrees);
 		},
 
+		/**
+		 * Deletes the selected slide
+		 * @method deleteSlide
+		 * @param {Object} event Click event
+		 */
 		deleteSlide : function(event) {
 			event.stopPropagation();
-
-			//Remove slide-mini
-			$("#" + app.clicked_slide.id).remove();
-			$("#slide-" + app.clicked_slide.id).remove();
-
-			this.hide();
 			app.slides.get(app.clicked_slide.id).destroy();
+			this.hide();
 		}
 	});
 });
